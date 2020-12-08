f099a42c5e8e0d05d094265bde733e0c5548be5e
wilbert <wilbertgeng@gmail.com>
2020-12-08 15:38:00 -0800

    Added post put patch and delete methods

---
+++ profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerialzer(serializers.Serializer):
+    """Serializes a name field for testing our APIview"""
+    name = serializers.CharField(max_length=10)
