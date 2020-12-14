db6c287d32199ce54b863a33623d19fecad4e690
wilbert <wilbertgeng@gmail.com>
2020-12-13 17:05:58 -0800

    Added login api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -5,11 +5,11 @@ class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-        if request.method in perssion.SAFE_METHOD:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.id == request.user.id
-        
+
 
 #
 #
